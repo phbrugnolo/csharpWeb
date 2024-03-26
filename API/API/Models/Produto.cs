ce2ca152d5f8a151f0dd1a4cb45ab12cf096940e
phbrugnolo <phbrugnolo@gmail.com>
2024-03-25 21:39:20 -0300

    Adicionando classe e filtro de busca para lista

---
+++ API/API/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace API.Models;
+
+public class Produto
+{
+    public Produto(string nome, string desc, string status, double preco)
+    {
+        Nome = nome;
+        Descricao = desc;
+        Status = status;
+        Preco = preco;
+    }
+
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public string? Status { get; set; }
+    public double Preco { get; set; }
+
+    
+}
+
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,20 +1,41 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> listaProd = new List<Produto>(){
-    new Produto("Celular", "Android"),
-    new Produto("Carro", "Foda"),
-    new Produto("Computador", "Tem 3090"),
-    new Produto("Mouse", "Ergonomico")
+
+List<Produto> listaProdutos = new List<Produto>(){
+    new Produto("Celular", "Android", "Active", 1700.00),
+    new Produto("Carro", "Foda", "Inactive", 20000.00),
+    new Produto("Computador", "Tem 3090", "Active", 7850.99),
+    new Produto("Mouse", "Ergonomico", "Active", 10.99)
 
 };
 
 
 //End Points
-app.MapPost("/api/produtos", () => "Api ruim com watch!");
+//Cadastrar um produto na lista
+// a) Atraves das informações da url
+// b) Atraves das informações no corpo da requisação
+// Realizar as operações de alterções e remoção da lista
+app.MapPost("/api/produtos/cadastrar", () => "Api com watch desfuncional!");
 
-app.MapGet("/api/produtos", () => listaProd);
+//GET  http://localhost:{porta}/api/produtos
+app.MapGet("/api/produtos/listar", () => listaProdutos);
+
+//GET  http://localhost:{porta}/api/buscar{product.nome}
+app.MapGet("/api/produtos/buscar/{nome}", ([FromRoute] string nome) =>
+{
+    //Endpoint com várias linhas de código 
+    for (int i = 0; i < listaProdutos.Count; i++)
+    {
+        if (listaProdutos[i].Nome == nome)
+        {
+            return Results.Ok(listaProdutos[i]);
+        }
+    }
+    return Results.NotFound();
+});
 
 app.Run();
-
-public record Produto(string nome, string desc);
